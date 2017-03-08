ec9a2fd410d83410cf6ceb9d1c8571c656166f0c
lamtoancali <lamtoancali@yahoo.com>
2017-03-08 11:14:48 -0700

    Initial Commit

---
+++ src/test/java/com/mycompany/helloglashfishmaven/NameStorageBeanTest.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helloglashfishmaven;
+
+import org.junit.After;
+
+import org.junit.AfterClass;
+
+import org.junit.Before;
+
+import org.junit.BeforeClass;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+
+/**
+
+ *
+
+ * @author Gary A. Stafford
+
+ */
+
+public class NameStorageBeanTest {
+
+
+
+    private final NameStorageBean nameStorageBean = new NameStorageBean();
+
+
+
+    public NameStorageBeanTest() {
+
+    }
+
+
+
+    @BeforeClass
+
+    public static void setUpClass() {
+
+    }
+
+
+
+    @AfterClass
+
+    public static void tearDownClass() {
+
+    }
+
+
+
+    @Before
+
+    public void setUp() {
+
+    }
+
+
+
+    @After
+
+    public void tearDown() {
+
+    }
+
+
+
+    /**
+
+     * Test of getName method, of class NameStorageBean.
+
+     *
+
+     * @throws java.lang.Exception
+
+     */
+
+    @Test
+
+    public void testGetName() throws Exception {
+
+        System.out.println("getName");
+
+        String result = nameStorageBean.getName();
+
+        assertNotNull(result);
+
+    }
+
+
+
+    /**
+
+     * Test of setName method, of class NameStorageBean.
+
+     *
+
+     * @throws java.lang.Exception
+
+     */
+
+    @Test
+
+    public void testSetName() throws Exception {
+
+        System.out.println("setName");
+
+        String expResult = "Test";
+
+        nameStorageBean.setName(expResult);
+
+        String result = nameStorageBean.getName();
+
+        assertEquals(expResult, result);
+
+    }
+
+}
